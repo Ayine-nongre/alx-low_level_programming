9e8bac63c1bf6c6b51a5fd5d7f9bc2811b4f7f05
Ayine-nongre <eugeneatinbire@gmail.com>
2023-02-17 21:21:11 +0000

    Adding 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+	char m;
+
+	n = 48;
+	m = 48;
+	while (n < 58)
+	{
+		m = 48;
+		while (m < 58)
+		{
+			if (n != m && n < m)
+			{
+				putchar(n);
+				putchar(m);
+				if (n == 57 && m == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			m++;
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
