b6e1e817ca0480ca590bd19da6a382a16fdfe596
Ayine-nongre <eugeneatinbire@gmail.com>
2023-02-21 21:06:08 +0000

    Editing 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,17 @@
 /**
  * _islower - prints 0 if letter is uppercase and 1 if letter is lowercase
  *
- * Returns: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int _islower(int b)
 {
 	if (b > 96 && b < 123)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 	_putchar('\n');
 }
