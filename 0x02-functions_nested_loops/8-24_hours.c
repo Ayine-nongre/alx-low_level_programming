1274b0c439ea8abaf684a3407b443224ad65f855
Ayine-nongre <eugeneatinbire@gmail.com>
2023-02-22 08:54:59 +0000

    Adding 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.c"
+/**
+ * jack_bauer - prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a < 51; a++)
+	{
+		for (b = 48; b < 58; b++)
+		{
+			for (c = 48; c < 54; c++)
+			{
+				for (d = 48; d < 58; d++)
+				{
+					if (a == 50 && b == 52)
+					{
+						break;
+					}
+					_putchar(a);
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+				}
+			}
+		}
+	}
+}
