1f1675e874d6f679716f4d26997c12c565574fd7
Ayine-nongre <eugeneatinbire@gmail.com>
2023-02-27 08:22:43 +0000

    Adding 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,5 +8,17 @@
 
 void _puts(char *str)
 {
-	printf("%s\n", str);
+	int count;
+
+	count = 0;
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++
+	}
 }
