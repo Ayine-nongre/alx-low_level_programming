add8144c719d19975a8cec34c980f99e371bab1f
Ayine-nongre <eugeneatinbire@gmail.com>
2023-02-27 07:49:52 +0000

    Adding 0-reset_to_98.c and readme

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes the value a variable points at to 98
+ * @n: parameter to change
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
