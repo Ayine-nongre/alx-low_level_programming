d0776945098a772b3750f2ef0e6d2a79a79c818e
Ayine-nongre <eugeneatinbire@gmail.com>
2023-02-28 17:34:09 +0000

    Adding 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every odd char in a string
+ * @str: is the string parameter
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if(i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
