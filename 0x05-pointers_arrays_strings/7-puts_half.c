c86f4bedbf4672f6d440081d56aee7bf6e9d7bc7
Ayine-nongre <eugeneatinbire@gmail.com>
2023-02-28 18:20:44 +0000

    Adding 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of the char in a str
+ * @str: parameter to print
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int n, i;
+
+	n = 0;
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+
+	n = n / 2;
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
