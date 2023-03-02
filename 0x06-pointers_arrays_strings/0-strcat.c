a93c70361eccea52bca577d1f36c9d77273796f0
Ayine-nongre <eugeneatinbire@gmail.com>
2023-03-02 08:53:11 +0000

    Adding 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.c"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * Return: the concatenated string
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	int i, j;
+
+	i = j = 0;
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	while (j >= 0)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+			break;
+		i++;
+		j++;
+	}
+	return (dest);
+}
