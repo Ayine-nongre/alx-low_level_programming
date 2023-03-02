397145013a042b0154a6746f2782a71705aef09a
Ayine-nongre <eugeneatinbire@gmail.com>
2023-03-02 11:20:25 +0000

    Editing 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,9 +17,14 @@ char *_strncpy(char *dest, char *src, int n)
 		i++;
 	}
 
-	for (; j < n; j++)
+	for (; j < n && *(src + j) != '\0' ; j++)
 	{
 		*(dest + j) = *(src + j);
+	}
+
+	for (; j < n; j++)
+	{
+		*(dest + j) = '\0';
 	}
 	return (dest);
 }
