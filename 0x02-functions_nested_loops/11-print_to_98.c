3995638a38b9f1066d599ac912ccdf4bb2adb30d
Ayine-nongre <eugeneatinbire@gmail.com>
2023-03-12 14:50:47 +0000

    Editing 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,12 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * print_to_98 - prints to 98
  * @n: parameter to start printing from
  * Return: the numbers
  */
+
 void print_to_98(int n)
 {
 	if (n > 98)
