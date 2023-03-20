ddd4a58c50f01ff5c52a62c1d08a1b754f41c83e
Ayine-nongre <eugeneatinbire@gmail.com>
2023-03-20 14:17:37 +0000

    Adding dog.h and README.md

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - short description
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
