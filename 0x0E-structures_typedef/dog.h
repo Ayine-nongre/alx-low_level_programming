8427cce455ffbfbfde8b09d2db247662103b3371
Ayine-nongre <eugeneatinbire@gmail.com>
2023-03-20 15:59:25 +0000

    Editing dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,20 +2,25 @@
 #define _DOG_H_
 
 /**
+ * dog_t - Typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+/**
  * struct dog - short description
  * @name: dog's name
  * @age: dog's age
  * @owner: owner's name
- * dog_t - Typedef for struct dog
  *
  * Description: Description: struct called "dog" that stores its name, its age
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
 
 #endif
