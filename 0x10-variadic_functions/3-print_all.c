e7c76c832984438408353b69c53fd1840ea80a95
Ayine-nongre <eugeneatinbire@gmail.com>
2023-03-23 18:24:52 +0000

    Adding 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,11 +10,44 @@
 void print_all(const char * const format, ...)
 {
 	va_list params;
-	int i;
+	unsigned int i;
+	char *str;
 
 	i = 0;
 
+	va_start(params, format);
+
 	while (*(format + i) != '\0')
+	{
+		if (format[i] == 'c' || format[i] == 'i' ||
+				format[i] == 'f' || format[i] == 's')
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c", va_arg(params, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(params, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(params, double));
+					break;
+				default:
+					str = va_arg(params, char *);
+					if (str == NULL)
+					{
+						printf("nil");
+						break;
+					}
+					printf("%s", str);
+			}
+			if (format[i + 1] != '\0')
+				printf(", ");
+		}
 		i++;
+	}
 
-
+	printf("\n");
+	va_end(params);
+}
