f17923d042ca305d789b880af9a584b586af3f32
Ayine-nongre <eugeneatinbire@gmail.com>
2023-03-30 22:04:52 +0000

    Adding 0-print_list.c,lists.h, 1-list_len.c

---
+++ 0x12-singly_linked_list/2-add_node.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node at the beginning of a list
+ * @head: list_t param
+ * @str: string param
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
