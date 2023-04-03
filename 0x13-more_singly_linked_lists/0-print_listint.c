b57dcdcf914311383750110d12ee82044f4496f4
Ayine-nongre <eugeneatinbire@gmail.com>
2023-04-03 10:13:34 +0000

    Adding lists.h, and 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: list item
+ * Return: size_t param
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t n;
+	listint_t temp;
+
+	temp = malloc(sizeof(listint));
+	if (!temp)
+	{
+		free(temp);
+		return (1);
+	}
+
+	*temp = *h;
+	n = 0;
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		n++;
+	}
+
+	return (n);
+}
