275bacce24073e0ec6c29be23a785eb2dbc7f510
Ayine-nongre <eugeneatinbire@gmail.com>
2023-04-04 16:40:49 +0000

    Editing task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,4 +7,17 @@
 
 void free_listint2(listint_t **head)
 {
+	listint_t *temp;
+	listint_t *new;
+
+	if (head != NULL)
+	{
+		new = *head;
+		while ((temp = new) != NULL)
+		{
+			new = new->next;
+			free(temp);
+		}
+		head = NULL;
+	}
 }
