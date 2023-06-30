d5bc08b29502ce230033f0fdc2c0fa9b5720dab8
Ayine-nongre <eugeneatinbire@gmail.com>
2023-06-30 18:54:41 +0000

    Adding 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,4 +9,42 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *new;
+	list_t *temp;
+	size_t length = 0;
+
+	new = malloc(sizeof(list_t));
+	(void)temp;
+
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+	if (!(new->str))
+	{
+		free(new);
+		return (NULL);
+	}
+
+	while (*(str + length) != '\0')
+		length++;
+
+	new->len = length;
+	new->next = NULL;
+
+	temp = *head;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+
+	return (*head);
 }
