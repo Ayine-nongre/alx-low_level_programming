746036a3ca2ccae66be7d1f8a8e515164ec21452
Ayine-nongre <eugeneatinbire@gmail.com>
2023-07-10 21:51:36 +0000

    Editing 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,14 +1,53 @@
 #include "main.h"
 
+/**
+ * copy_files - driver code to copy files
+ * @num_read: int
+ * @num_write: int
+ * @file_from: int
+ * @file_to: int
+ * @str: string
+ * @from: string
+ * @to: string
+ */
+
+void copy_files(int num_read, int num_write, int file_from,
+		int file_to, char *str, char *from, char *to)
+{
+	while (num_read == 1024)
+	{
+		num_read = read(file_from, str, 1024);
+		if (num_read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", from);
+			exit(98);
+		}
+
+		num_write = write(file_to, str, 1024);
+		if (num_write == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", to);
+			exit(99);
+		}
+	}
+}
+
+/**
+ * main - main entry
+ * @ac: arg count
+ * @av: array of args
+ * Return: int
+ */
+
 int main(int ac, char **av)
 {
 	int file_from, file_to, closer1, closer2;
-	ssize_t num_read = 1024, num_write;
+	ssize_t num_read = 1024, num_write = 0;
 	char str[1024];
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND,
@@ -24,21 +63,8 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
 		exit(99);
 	}
-	while (num_read == 1024)
-	{
-		num_read = read(file_from, str, 1024);
-		if (num_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[1]);
-			exit(98);
-		}
-		num_write = write(file_to, str, 1024);
-		if (num_write == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]);
-			exit(99);
-		}
-	}
+
+	copy_files(num_read, num_write, file_from, file_to, str, av[1], av[2]);
 
 	closer1 = close(file_to);
 	if (closer1 == -1)
@@ -46,6 +72,7 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
+
 	closer2 = close(file_from);
 	if (closer2 == -1)
 	{
