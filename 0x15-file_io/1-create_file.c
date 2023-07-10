3a37c42931636764c26911721e17487b0c332bb6
Ayine-nongre <eugeneatinbire@gmail.com>
2023-07-10 13:04:32 +0000

    Adding truncate flag

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fp = creat(filename, S_IWUSR | S_IRUSR);
+	fp = creat(filename, S_IWUSR | S_IRUSR | O_TRUNC);
 
 	while (*(text_content + n) != '\0')
 		n++;
@@ -27,5 +27,5 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	close(fp);
-	return (num);
+	return (1);
 }
