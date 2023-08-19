f130190d9d7bd7ffc6a69081c3d060367f756f3c
Ayine-nongre <eugeneatinbire@gmail.com>
2023-08-19 10:11:19 +0000

    Adding task 0

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+#endif
+
