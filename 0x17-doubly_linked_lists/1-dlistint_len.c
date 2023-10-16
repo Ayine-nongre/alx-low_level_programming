82a41b561c469bffa2ce61f5f48e5721732236f3
Ayine-nongre <eugeneatinbire@gmail.com>
2023-10-16 19:44:57 +0000

    Adding task for doubly linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,29 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in a linked list
- * @h: doubly linked list
- * Return: size_t param
+ * dlistint_len - returns the number of elements in
+ * a double linked list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
  */
-
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t num
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
 	while (h != NULL)
 	{
+		count++;
 		h = h->next;
-		num++;
 	}
 
-	return (num);
+	return (count);
 }
