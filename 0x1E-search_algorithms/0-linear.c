897eb73f67b0a90ddac98692b046e045798f3b03
Ayine-nongre <eugeneatinbire@gmail.com>
2023-12-11 11:55:37 +0000

    Adding task 1

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - An algo to search for a number in an array
+ * @array: pointer to array of numbers
+ * @size: size of array
+ * @value: number to find
+ * Return: number index or -1 if not found
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld]: [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+ALX search algorithm tasks
