3c41ce9750f6081efacdf3a770c612c98d35cdb0
Ayine-nongre <eugeneatinbire@gmail.com>
2023-12-13 09:52:29 +0000

    Fixing typo error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,17 +13,15 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
-	int index = 0;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		index = i;
 		printf("Value checked array[%ld]: [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return (index);
+			return (i);
 	}
 
 	return (-1);
