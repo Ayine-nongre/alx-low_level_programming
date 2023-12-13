b5efc2ab5453dd7e736054a295fa6bd52f24a2db
Ayine-nongre <eugeneatinbire@gmail.com>
2023-12-13 09:39:35 +0000

    Corrections made

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,15 +13,17 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
+	int index = 0;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
+		index = i;
 		printf("Value checked array[%ld]: [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return (int(i));
+			return (index);
 	}
 
 	return (-1);
